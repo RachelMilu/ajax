7af48f8c4e3c30c77db94ef2c829e4ded132e36b
longjia <longjiarun@qq.com>
2016-07-02 18:12:26 +0800

    增加配置项

--- dist/ajax.js
+++ dist/ajax.js
@@ -1,21 +1,41 @@
 !(function() {
 
     var ajax = function(opts) {
-        var xhr = new XMLHttpRequest()
+        var xhr = new XMLHttpRequest(),
+            type = opts.type,
+            url = opts.url
 
-        xhr.open(opts.type.toUpperCase() || 'GET', opts.url, true)
+        opts.cache && (url += (url.indexOf('?') === -1 ? '?' : '&') + '_=' + Date.now())
+
+        xhr.open(type && type.toUpperCase() || 'GET', url, true)
 
         xhr.onreadystatechange = function() {
+            var result,
+                error = opts.error,
+                success = opts.success
+
             if (xhr.readyState == 4) {
                 if (xhr.status >= 200 && xhr.status < 300 || xhr.status == 304) {
-                    opts.success && opts.success(xhr.responseText, xhr)
+                    var result = xhr.responseText
+
+                    if (opts.dataType === 'json') {
+                        try {
+                            result = JSON.parse(result)
+                        } catch (e) {
+                            error && error(e, 'parsererror', xhr)
+                            return
+                        }
+                    }
+
+                    success && success(result, xhr)
                 } else {
-                    opts.error && opts.error(xhr.statusText || null, xhr.status ? 'error' : 'abort', xhr)
+                    error && error(xhr.statusText || null, xhr.status ? 'error' : 'abort', xhr)
                 }
             }
         }
 
-        var xhrFields = opts.xhrFields,name
+        var xhrFields = opts.xhrFields,
+            name
         if (xhrFields) {
             for (name in xhrFields) xhr[name] = xhrFields[name]
         }
